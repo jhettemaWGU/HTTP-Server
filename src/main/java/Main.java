e52438304f4eb0a4bf7e5c6096b43f016264f64e
Jonathan Hettema <jhettem@wgu.edu>
2024-09-09 16:59:07 -0700

    Respond with body complete.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,17 +17,33 @@ public class Main {
        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
        String line = reader.readLine();
        System.out.println(line);
-       String[] HttpRequest = line.split(" ", 0);
+       String[] HttpRequest = line.split(" ");
        OutputStream output = clientSocket.getOutputStream();
-         if (HttpRequest[1].equals("/")) {
-           output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-         } else {
-           output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-         }
+       String requestPath = HttpRequest[1];
+
+       if (requestPath.startsWith("/echo/")) {
+         String echoString = requestPath.substring("/echo/".length());
+
+         output.write("HTTP/1.1 200 OK\r\n".getBytes());
+         output.write("Content-Type: text/plaing\r\n".getBytes());
+         output.write("Content-Length: " + echoString.length() + "\r\n".getBytes());
+         output.write("\r\n".getBytes());
+         output.write(echoString.getBytes());
+       } else {
+         output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+       }
+
        System.out.println("accepted new connection");
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
+     } finally {
+       try {
+         if (serverSocket != null) serverSocket.close();
+         if (clientSocket != null) clientSocket.close();
+       } catch (IOException e) {
+         System.out.println("Error closing sockets: " + e.getMessage());
+       }
      }
   }
 }
