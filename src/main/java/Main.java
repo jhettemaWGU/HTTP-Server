6e371c61b4440b21b28e67d9a22e001824614225
Jonathan Hettema <jhettem@wgu.edu>
2024-09-09 15:54:28 -0700

    Exact url path complete.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,32 @@
-import java.io.IOException;
+package main.java;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    //System.out.println("Logs from your program will appear here!");
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
      try {
        serverSocket = new ServerSocket(4221);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
+       InputStream input = clientSocket.getInputStream();
+       BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+       String line = reader.readLine();
+       System.out.println(line);
+       String[] HttpRequest = line.split(" ", 0);
+       OutputStream output = clientSocket.getOutputStream();
+       output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+         if (HttpRequest[1].equals("/")) {
+           output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+         } else {
+           output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+         }
        System.out.println("accepted new connection");
-       clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
