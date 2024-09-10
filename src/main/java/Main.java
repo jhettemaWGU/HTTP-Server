b9f8c5a6362cb623d9ee1e822f46f88def8990e4
Jonathan Hettema <jhettem@wgu.edu>
2024-09-09 23:33:48 -0700

    Concurrent connections complete.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,56 +30,58 @@ public class Main {
        }
      }
   }
-}
 
-private static void handleClient(Socket clientSocket) {
-  try (clientSocket) {
-    InputStream input = clientSocket.getInputStream();
-    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-    String line = reader.readLine();
-    System.out.println(line);
-    String[] HttpRequest = line.split(" ");
-    OutputStream output = clientSocket.getOutputStream();
-    String requestPath = HttpRequest[1];
-    String userAgent = null;
+  private static void handleClient(Socket clientSocket) {
+    try (clientSocket) {
+      InputStream input = clientSocket.getInputStream();
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      String line = reader.readLine();
+      System.out.println(line);
+      String[] HttpRequest = line.split(" ");
+      OutputStream output = clientSocket.getOutputStream();
+      String requestPath = HttpRequest[1];
+      String userAgent = null;
 
-    if (requestPath.equals("/")) {
-      output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      if (requestPath.equals("/")) {
+        output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
 
-    } else if (requestPath.startsWith("/echo/")) {
-      String echoString = requestPath.substring("/echo/".length());
-      output.write("HTTP/1.1 200 OK\r\n".getBytes());
-      output.write("Content-Type: text/plain\r\n".getBytes());
-      String lengthParam = "Content-Length: " + echoString.length() + "\r\n";
-      output.write(lengthParam.getBytes());
-      output.write("\r\n".getBytes());
-      output.write(echoString.getBytes());
-    } else if (requestPath.startsWith("/user-agent")) {
-      while ((line = reader.readLine()) != null && !line.isEmpty()) {
-        System.out.println(line);
+      } else if (requestPath.startsWith("/echo/")) {
+        String echoString = requestPath.substring("/echo/".length());
+        output.write("HTTP/1.1 200 OK\r\n".getBytes());
+        output.write("Content-Type: text/plain\r\n".getBytes());
+        String lengthParam = "Content-Length: " + echoString.length() + "\r\n";
+        output.write(lengthParam.getBytes());
+        output.write("\r\n".getBytes());
+        output.write(echoString.getBytes());
+      } else if (requestPath.startsWith("/user-agent")) {
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+          System.out.println(line);
 
-        if (line.startsWith("User-Agent: ")) {
-          userAgent = line.substring("User-Agent: ".length()).trim();
-          break;
+          if (line.startsWith("User-Agent: ")) {
+            userAgent = line.substring("User-Agent: ".length()).trim();
+            break;
+          }
         }
+      } else {
+        output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
       }
-    } else {
-      output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
-    }
 
-    if (userAgent != null) {
-      String httpResponse =
-              "HTTP/1.1 200 OK\r\n" +
-                      "Content-Type: text/plain\r\n" +
-                      "Content-Length: " + userAgent.length() + "\r\n\r\n" +
-                      userAgent;
-      output.write(httpResponse.getBytes());
-    } else if (requestPath.startsWith("/user-agent")){
-      String httpResponse = "HTTP/1.1 400 Bad Request\r\n\r\nUser-Agent header missing\r\n";
-      output.write(httpResponse.getBytes());
+      if (userAgent != null) {
+        String httpResponse =
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: text/plain\r\n" +
+                        "Content-Length: " + userAgent.length() + "\r\n\r\n" +
+                        userAgent;
+        output.write(httpResponse.getBytes());
+      } else if (requestPath.startsWith("/user-agent")){
+        String httpResponse = "HTTP/1.1 400 Bad Request\r\n\r\nUser-Agent header missing\r\n";
+        output.write(httpResponse.getBytes());
+      }
+      clientSocket.close();
+    } catch (IOException e) {
+      System.out.println("Error handling client: " + e.getMessage());
     }
-    clientSocket.close();
-  } catch (IOException e) {
-    System.out.println("Error handling client: " + e.getMessage());
   }
 }
+
+
