9b650ccd6bcc5f72bc23f7ed378760e9b1ca9212
Jonathan Hettema <jhettem@wgu.edu>
2024-09-09 23:34:30 -0700

    Concurrent connections complete.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,6 @@ public class Main {
   public static void main(String[] args) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     //System.out.println("Logs from your program will appear here!");
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
      try (ServerSocket serverSocket = new ServerSocket(4221)) {
        serverSocket.setReuseAddress(true);
        System.out.println("Server is running and waiting for connections.");
